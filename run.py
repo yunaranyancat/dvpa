8f5e789880b0acd48f42634f0dc820b48cb8efb0
M Zulfiqar W <mzulfiqar.wardi@gmail.com>
2022-08-14 15:47:55 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,40 @@
+from flask import Flask,request,jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import json
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+
+users = {
+    "admin": generate_password_hash("admin"),
+    "susan": generate_password_hash("susan")
+}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.current_user())
+
+@app.route('/register', methods = ['POST'])
+def register():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)
+    if username in users:
+        abort(400)
+    users[username] = generate_password_hash(password)
+    return jsonify({'created':True}), 201
+
+
+if __name__ == '__main__':
+    app.run()
