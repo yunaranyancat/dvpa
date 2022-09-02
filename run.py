497f34b7ad0b08162c61e080cf31824669f28387
M Zulfiqar W <mzulfiqar.wardi@gmail.com>
2022-09-02 23:04:31 +0800

    insecure file upload

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from flask import Flask,request,jsonify
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import os
 import json
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+UPLOADS = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOADS
 
 users = {
     "admin": generate_password_hash("admin"),
@@ -33,12 +35,31 @@ def register():
     if username in users:
         abort(400)
     users[username] = generate_password_hash(password)
-    return jsonify({'created':True}), 201
+    return jsonify({'created' : True}), 201
 
 @app.route('/greet/<user>')
 def greet(user):
     output = subprocess.getoutput("echo Hello " + user)
-    return jsonify({'result':output}), 200
+    return jsonify({'result' : output}), 200
+
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    if 'files[]' not in request.files:
+        return jsonify({'error' : 'Unsupported format'}), 400
+
+    files = request.files.getlist('files[]')
+
+    errors = {}
+    success = False
+
+    for file in files:
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        success = True
+
+    if success:
+        return jsonify({'success' : 'File has been successfully uploaded'}), 200
+    else:
+        return jsonify({'error' : 'Server encountered some error'}), 500
 
 if __name__ == '__main__':
     app.run()
